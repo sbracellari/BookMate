88db0f818961287430394af8e4cc24d82296d645
FrankieGirardot <girardot@oakland.edu>
2020-01-29 18:01:45 -0500

    adds initial backend files #1

---
+++ src/test/java/edu/project/ebooks/EbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.project.ebooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
